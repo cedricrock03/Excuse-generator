b5aa85c86b1b07f99810f0354944f13e86ac7975
cedricrock03 <153559682+cedricrock03@users.noreply.github.com>
2024-01-05 16:23:28 +0000

    push to repo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML= generateExcuse();
+  document.querySelector("#button").addEventListener("click", generateExcuse);
 };
+function generateExcuse() {
+  let who = [" Dog ", " Cat", " Turtle", " bird"];
+  let action = [" ate", " burned", " ripped the", " threw", " broke"];
+  let what = [" homework", " keys", " car", " my toy"];
+  let when = [" before the class", " right on time", " during lunch"];
+
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let actionIndx = Math.floor(Math.random() * action.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
+  console.log("clicked button");
+
+  let newphrase =
+    who[whoIndx] +
+    "" +
+    action[actionIndx] +
+    "" +
+    what[whatIndx] +
+    "" +
+    when[whenIndx];
+  document.querySelector("#excuse").innerHTML = newphrase;
+}
